3565b52b32014f15e63cbfa4d4e8b0b5b26b003a
XervE <xerve.dev@gmail.com>
2026-01-29 01:15:31 +0100

    a

--- 2nd Quarter/ejercicio1_psp/src/lanzamientoPOOConcurrente/LanzarProceso.java
+++ 2nd Quarter/ejercicio1_psp/src/lanzamientoPOOConcurrente/LanzarProceso.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 public class LanzarProceso {
     public static int contador_proceso = 0;
     ProcessBuilder process;
-    LanzarProceso(String ruta){
-        process = new ProcessBuilder(ruta);
+    public String ruta;
+    public int numero;
+    
+    LanzarProceso(String ruta, int numero){
+        this.ruta = ruta;
+        this.numero = numero;
         contador_proceso++;
     }
     
     public void ejecutar(int numero){
         System.out.println("Ejecutando el proceso numero " + numero);
+        process = new ProcessBuilder(this.ruta);
         try {
             process.start();
         } catch (IOException e) {
--- 2nd Quarter/ejercicio1_psp/src/lanzamientoPOOConcurrente/Principal.java
+++ 2nd Quarter/ejercicio1_psp/src/lanzamientoPOOConcurrente/Principal.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
-final static String ruta = "\"C:\\Program Files\\Adobe\\Acrobat DC\\Acrobat\\Acrobat.exe\"";   
+// final static String ruta = "\"C:\\Program Files\\Adobe\\Acrobat DC\\Acrobat\\Acrobat.exe\"";
+// final static String RUTA = "\"C:\\Program Files\\WindowsApps\\Microsoft.WindowsNotepad_11.2312.18.0_x64__8wekyb3d8bbwe\\Notepad\\Notepad.exe\"";
+final static String RUTA = "\"C:\\\\Program Files\\\\Oracle\\\\VirtualBox\\\\VirtualBox.exe\"";
 final static int INI_INT = 0;
 public static void main(String[] args) {
     System.out.println("Inicio");
@@ -13,17 +15,15 @@ public static void main(String[] args) {
         ArrayList<LanzarProceso> lista_procesos = new ArrayList<LanzarProceso>();
 		System.out.println("Introduce el número de veces que quieres lanzar el proceso");
         numero = entrada.nextInt();
-        for(int i = INI_INT; i<numero; i++){
-            LanzarProceso proceso = new LanzarProceso(ruta);
+        for(int i = INI_INT; i <numero; i++){
+            LanzarProceso proceso = new LanzarProceso(RUTA, i+1);
             lista_procesos.add(proceso);  
         }
-        int contador = INI_INT;
 
         for (LanzarProceso proceso : lista_procesos) {
-            
-            contador++;
-            proceso.ejecutar(contador);
+            proceso.ejecutar(proceso.numero);
         }
         entrada.close();
+        
 }
 }
