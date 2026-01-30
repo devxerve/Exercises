86713e30940162e440015e13f4d260b9944cb11b
XervE <xerve.dev@gmail.com>
2026-01-30 11:28:53 +0100

    a

--- 2nd Quarter/ejercicio1_psp/src/lanzamientoPOOConcurrente/Principal.java
+++ 2nd Quarter/ejercicio1_psp/src/lanzamientoPOOConcurrente/Principal.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
-// final static String ruta = "\"C:\\Program Files\\Adobe\\Acrobat DC\\Acrobat\\Acrobat.exe\"";
-// final static String RUTA = "\"C:\\Program Files\\WindowsApps\\Microsoft.WindowsNotepad_11.2501.31.0_x64__8wekyb3d8bbwe\\Notepad\\Notepad.exe\"";
+// final static String RUTA = "C:\\Program Files\\WindowsApps\\Microsoft.WindowsNotepad_11.2312.18.0_x64__8wekyb3d8bbwe\\Notepad\\Notepad.exe";
 final static String RUTA = "\"C:\\Program Files\\Oracle\\VirtualBox\\VirtualBox.exe\"";
 final static int INI_INT = 0;
 public static void main(String[] args) {
@@ -15,6 +14,7 @@ public static void main(String[] args) {
         ArrayList<LanzarProceso> lista_procesos = new ArrayList<LanzarProceso>();
 		System.out.println("Introduce el número de veces que quieres lanzar el proceso");
         numero = entrada.nextInt();
+
         for(int i = INI_INT; i <numero; i++){
             LanzarProceso proceso = new LanzarProceso(RUTA, i+1);
             lista_procesos.add(proceso);  
@@ -24,6 +24,5 @@ public static void main(String[] args) {
             proceso.ejecutar(proceso.numero);
         }
         entrada.close();
-        
 }
 }
