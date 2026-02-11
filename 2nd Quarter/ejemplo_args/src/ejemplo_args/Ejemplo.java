fbc4370ffa64c9e8d79e2f2979ac7c6a14d78872
XervE <aviercerverarodriguez@gmail.com>
2026-02-11 09:04:35 +0100

    a

---
+++ 2nd Quarter/ejemplo_args/src/ejemplo_args/Ejemplo.java
@@ -0,0 +1,17 @@
+package ejemplo_args;
+
+public class Ejemplo {
+public static void main(String[] args) {
+	if(args.length < 2) {
+		System.out.println("[MAIN] Se esperaban mas de 2 argumentos de entrada");
+		System.exit(-3);
+	}else {
+		String nombre = args[0];
+		String palabra = args[1];
+		System.out.println(palabra + nombre);
+		System.out.println("Programa finalizado correctamente");
+		System.exit(0);
+	}
+	
+}
+}
