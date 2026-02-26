a866a874d3412680aaad9416730ed834ab3cbb49
XervE <aviercerverarodriguez@gmail.com>
2026-02-26 11:20:11 +0100

    Refactorizar estructura

--- 2nd Quarter/psp_ej4_comunicacionProcesosSumadores/src/paqueteSumador/Sumador.java
+++ 2nd Quarter/psp_ej4_comunicacionProcesosSumadores/src/paqueteSumador/Sumador.java
@@ -1,13 +1,16 @@
 package paqueteSumador;
 
 public class Sumador {
-    static int sumar(int num1,int num2){
-        return num1 + num2;
-    }
     public static void main(String[] args) {
-        if(args.length !=2){
-            System.out.println("Error, se necesitan 2 números");
+       
+        if(args.length == 0){
+            System.out.println("Se esperaban 2 argumentos de entrada");
+            System.exit(0);
         }
-        sumar(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+        
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int suma = num1 + num2;
+        System.out.println(suma);  
     }
 }
--- 2nd Quarter/psp_ej4_comunicacionProcesosSumadores/src/principal/Principal.java
+++ 2nd Quarter/psp_ej4_comunicacionProcesosSumadores/src/principal/Principal.java
@@ -1,37 +1,52 @@
 package principal;
-
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Principal {
+    static int salto = 0;
+    
     public static void lanzarSumador(String num1, String num2, String fichero){
         String ruta = "paqueteSumador.Sumador";
         File dir = new File("./bin");
-        ProcessBuilder pb = new ProcessBuilder("java",ruta,num1,num2);
+        ProcessBuilder pb = new ProcessBuilder("java", ruta, num1, num2);
         try{
             pb.directory(dir);
             pb.redirectOutput(new File(fichero));
             Process proceso = pb.start();
-        }catch(IOException e){
+            Thread.sleep(1000);
+        }catch(IOException | InterruptedException e){
             e.printStackTrace();
         }
     }
-    public static String generarRuta(){
-        
+    
+    public static File generarFile(int proceso){
+        Date fecha = new Date();
+        SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd-HH_mm_ss");
+        File fichero =  new File("resultado_proceso" + (proceso+1)+"_Fecha"+dtf.format(fecha) + ".txt");
+        return fichero;
     }
+    
     public static void main(String[] args) {
-        String num1 = null;
-        String num2 = null;
         Scanner entrada = new Scanner(System.in);
         System.out.println("Introduce 2 números para sumar");
-        System.out.println("Número 1");
-        num1 = Integer.toString(entrada.nextInt());
-        System.out.println("Número 2");
-        num2 = Integer.toString(entrada.nextInt());
-        lanzarSumador(num1, num2, );
+        System.out.print("Número 1: ");
+        int numero1 = entrada.nextInt();
+        System.out.print("Número 2: ");
+        int numero2 = entrada.nextInt();
         
+        salto = ((numero2 - numero1) / 4) - 1;  
         
-
+        int inferior = numero1;
+        int superior = numero2;
+        for(int i = 0; i < 4; i++){
+            File fichero = generarFile(i);
+            lanzarSumador(Integer.toString(inferior), Integer.toString(superior), fichero.getPath());
+            inferior = inferior + salto + 1 ;
+        }
+        entrada.close();
+        System.out.println("4 procesos lanzados con salto = " + salto);
     }
 }
